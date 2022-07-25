75be21f688a57dd8903d24611e884654e4a91dc1
GZ <l2241883@gmail.com>
2022-07-25 14:55:43 +0800

    add craco webpack config

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const CracoLessPlugin = require('craco-less');
+const path = require('path');
 
 module.exports = {
   plugins: [
@@ -7,11 +8,42 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { '@primary-color': '#1DA57A' },
+            modifyVars: {
+              '@primary-color': '#1B9AEE',
+              '@success-color': '#15AD31',
+              '@processing-color': '#1B9AEE',
+              '@error-color': '#E62412',
+              '@highlight-color': '#E62412',
+              '@warning-color': '#FA8C15',
+              '@text-color': '#212529',
+              '@text-color-secondary': '#495057',
+              '@heading-color': '#212529',
+            },
             javascriptEnabled: true,
           },
         },
       },
     },
   ],
+  webpack: {
+    //修改打包文件夹
+    configure: (webpackConfig, { env, paths }) => {
+      paths.appBuild = webpackConfig.output.path = path.resolve('dist');
+      return webpackConfig;
+    }
+  },
+  //设置dev环境 proxy
+  devServer: {
+    hot: true,
+    proxy: {
+      '/admin/api/v1': {
+        target: 'http://localhost:8080/',
+      }
+    },
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/$/, to: '/index.html' },
+      ],
+    },
+  },
 };
