e96c08ffcb71f3c8c04ad53396a3c83bdc9a072c
GZ <l2241883@gmail.com>
2022-07-28 19:06:57 +0800

    fix notfound page

--- src/app/components/Result/NotFound.tsx
+++ src/app/components/Result/NotFound.tsx
@@ -6,16 +6,14 @@ import { useNavigate } from 'react-router-dom';
 
 
 export default function NotFound() {
-
     let navigate = useNavigate();
 
     return (
         <div>
             <Result
                 status="404"
-                title="404"
-                subTitle="Sorry, the page you visited does not exist."
-                extra={<Button type="primary" onClick={() => { navigate("/")}}>Back Home</Button>}
+                title="页面不存在"
+                extra={<Button type="primary" onClick={() => { navigate("/")}}>返回主页</Button>}
             />
         </div>
     )
