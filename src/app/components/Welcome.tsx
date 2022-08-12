1685aaa329aaae73e32471bc655abbd54cbb675b
GZ315200 <l2241883@gmail.com>
2022-08-12 21:09:09 +0800

    fix logo width

--- src/app/components/Welcome.tsx
+++ src/app/components/Welcome.tsx
@@ -7,7 +7,7 @@ export default function Welcome() {
     <div className="flex flex-col">
         <h1 className="text-4xl font-light">欢迎使用{APP_TITLE}</h1>
         <div className="mt-5 flex justify-center">
-            <img src={logo} alt="welcome" width={980} className="rounded-full" />
+            <img src={logo} alt="welcome" width={720} className="rounded-full" />
          </div>
     </div>
     
